54c7609a053cd3f1d6730d2ba1870e240f50d246
inderjit singh <inderjit>
2022-10-26 13:01:34 +1300

    initial commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-  
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -26,21 +27,33 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if(!user){
-        res.status(401).json("Wrong Credentials!");
+    if (!user) {
+      res.status(401).json("Wrong Credentials!");
     }
     const hashedpassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC
     );
-    const password = hashedpassword.toString(CryptoJS.enc.Utf8);
-    if(password !==req.body.password){
-        res.status(401).json("Wrong Credentials!")
-    }
-    res.status(200).json(user);
-    
+    const Originalpassword = hashedpassword.toString(CryptoJS.enc.Utf8);
+
+    Originalpassword !== req.body.password &&
+      res.status(401).json("Wrong Credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
-  }});
+  }
+});
 
 module.exports = router;
